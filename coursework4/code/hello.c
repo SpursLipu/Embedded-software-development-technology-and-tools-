9a1a5c8d72ecb1735aafb424a2e2f0e95f2328ea
luyi <ly_sspku@pku.edu.cn>
2020-04-18 14:52:28 +0800

    course4 images

---
+++ coursework4/code/hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+        int a=10;
+        int b=20;
+	int i=0;
+        int sum;
+        sum=a+b;
+        printf("sum=%d\n",sum);
+        for(i=0;i<5;i++)
+    {
+        printf("hello world,%d\n",i);     
+    }
+}
